7e568fa9cc89648227e530134c53679028e2060c
Jonathan <jonathan.copson@gmail.com>
2025-10-05 11:09:50 +1300

    Add controller name since it's not in the same namespace

--- app/src/Page/InterviewCandidatePage.php
+++ app/src/Page/InterviewCandidatePage.php
@@ -3,8 +3,10 @@
 namespace SSTechInterview\Page;
 
 use Page;
+use SSTechInterview\Controller\InterviewCandidateController;
 
 class InterviewCandidatePage extends Page
 {
-
+    //Set controller
+    private static $controller_name = InterviewCandidateController::class;
 }
